beb386ea7aebbb75733f60a3ca397d3e13759f0f
Phuongthao2k <56735225+Phuongthao2k@users.noreply.github.com>
2023-01-11 15:30:33 +0700

    first commit

---
+++ AppFood/AppFood/Views/ProductPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using AppFood.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppFood.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ProductPage : ContentPage
+    {
+        public ProductPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void txtCollection_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var category = e.CurrentSelection.FirstOrDefault() as Category;
+            if (category == null)
+            {
+
+                return;
+            }
+
+            await Navigation.PushModalAsync(new CategoryView(category));
+
+            ((CollectionView)sender).SelectedItem = null;
+        }
+    }
+}
---
+++ AppFood/AppFood/Views/SearchResultView.xaml.cs
@@ -0,0 +1,22 @@
+﻿using AppFood.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppFood.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SearchResultView : ContentPage
+    {
+        public SearchResultView(string searchText)
+        {
+            InitializeComponent();
+            this.BindingContext = new SearchResultViewModel(searchText);
+        }
+    }
+}
